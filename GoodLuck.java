81dcb38f298b6e91cade449a59369e9070056a4c
nm47622791 <58712845+nm47622791@users.noreply.github.com>
2019-12-19 13:54:29 +0800

    modifiedv1.01

--- GoodLuck.java
+++ GoodLuck.java
@@ -2,8 +2,8 @@
 public class GoodLuck {
 
 	public static void main(String[] args) {
-		System.out.println("Gook Luck");
-		System.out.println("Gook Luck");
+		System.out.println("Gook Luck1");
+		System.out.println("Gook Luck2");
 	}
 	
 }
